de8a591c2d4a55ef5e8dd71c89186e9708683881
Rikuba <rikuba@live.jp>
2021-04-19 23:55:11 +0900

    RunnerTestでカバーできるユニットテストを削除する

--- 09.wc_object/test/count_list_test.rb
+++ 09.wc_object/test/count_list_test.rb
@@ -5,30 +5,6 @@ require_relative '../lib/count_list'
 
 module Wc
   class CountListTest < Test::Unit::TestCase
-    test 'render 3 count values' do
-      counts = { lines: 3, words: 19, chars: 124 }
-      result = CountList.render(counts)
-      assert_equal '       3      19     124', result
-    end
-
-    test 'render 3 count values and label' do
-      counts = { lines: 3, words: 19, chars: 124 }
-      result = CountList.render(counts, 'count_list.rb')
-      assert_equal '       3      19     124 count_list.rb', result
-    end
-
-    test 'render 4 count values' do
-      counts = { lines: 3, words: 19, chars: 124, bytes: 124 }
-      result = CountList.render(counts)
-      assert_equal '       3      19     124     124', result
-    end
-
-    test 'render 1 count values' do
-      counts = { lines: 3 }
-      result = CountList.render(counts)
-      assert_equal '       3', result
-    end
-
     test 'render big counts' do
       counts = { lines: 12_345_678, words: 123_456_789, chars: 1_234_567_890 }
       result = CountList.render(counts, 'big_file')
