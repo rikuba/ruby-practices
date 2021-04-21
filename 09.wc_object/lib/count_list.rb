719464e94de17f1c6e3d4c100d2143f3f5dbbbb2
Rikuba <rikuba@live.jp>
2021-04-21 09:38:27 +0900

    自明な戻り値のnilを省略する

--- 09.wc_object/lib/count_list.rb
+++ 09.wc_object/lib/count_list.rb
@@ -6,7 +6,7 @@ module Wc
 
     def self.render(counts, label = nil)
       count_text = COUNT_TYPE_ORDER.filter_map do |count_type|
-        next nil unless counts.key?(count_type)
+        next unless counts.key?(count_type)
 
         count = counts[count_type]
         width = [count.to_s.size + 1, 8].max
